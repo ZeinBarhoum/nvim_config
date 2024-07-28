028509917781d46844baeb3d7f8ecfe897d69b47
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2024-07-28 15:23:36 +0300

    initial commit

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,63 @@
+-- for mapping only on the tree
+local function my_on_attach(bufnr)
+	local api = require "nvim-tree.api"
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+	vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function(_, opts)
+		require("nvim-tree").setup {
+			on_attach = my_on_attach,
+			renderer = {
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+
+					glyphs = {
+						default = "󰈚",
+						symlink = "",
+						folder = {
+							default = "",
+							empty = "",
+							empty_open = "",
+							open = "",
+							symlink = "",
+							symlink_open = "",
+							arrow_open = "",
+							arrow_closed = "",
+						},
+						git = {
+							unstaged = "✗",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
+						},
+					},
+				},
+			},
+		}
+	end,
+}

ff96f31f5fc5dc59ffc69c7c34900339ad175948
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2024-08-04 02:23:35 +0300

    Change lualine section c to full path filename

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,15 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true, -- Displays file status (readonly status, modified status)
+					path = 3,
+				},
+			},
+		},
+	},
 }

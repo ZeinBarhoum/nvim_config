d43c4405ee0095e19ce03f5c3ccf3e7cda42a488
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2025-03-11 17:01:37 +0300

    Add venv selector

---
+++ lua/plugins/venv_selector.lua
@@ -0,0 +1,25 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	},
+	lazy = false,
+	branch = "regexp", -- This is the regexp branch, use this for the new version
+	config = function()
+		require("venv-selector").setup({
+			settings = {
+				search = {
+					micromamba = {
+						command = "fd --full-path '^/home/zein/micromamba/envs/.*/python$' --type symlink",
+					},
+				},
+			},
+		})
+	end,
+	keys = {
+		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+	},
+}

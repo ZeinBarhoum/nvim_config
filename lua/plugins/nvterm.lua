53923fa953f69418dc12db1d51473bb3ae7a94b5
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2025-03-13 19:23:04 +0300

    resize nvterm floating terminal

--- lua/plugins/nvterm.lua
+++ lua/plugins/nvterm.lua
@@ -1,4 +1,24 @@
 return {
 	"zbirenbaum/nvterm",
-	opts = {},
+	config = function()
+		local width = 0.9
+		local height = width
+		local row = (1 - height) / 2
+		local col = (1 - width) / 2
+
+		require("nvterm").setup({
+			terminals = {
+				type_opts = {
+					float = {
+						relative = "editor",
+						row = row,
+						col = col,
+						width = width,
+						height = height,
+						border = "single",
+					},
+				},
+			},
+		})
+	end,
 }

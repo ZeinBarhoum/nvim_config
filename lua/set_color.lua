d84d519264d57485512e22d2327ac416a976e45e
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2025-04-17 15:55:16 +0300

    color

--- lua/set_color.lua
+++ lua/set_color.lua
@@ -1,5 +1,4 @@
--- vim.cmd.colorscheme("catppuccin-mocha")
-vim.cmd.colorscheme("github_dark_high_contrast")
+vim.cmd.colorscheme("NeoSolarized")
 vim.cmd.hi("Comment gui=none")
 local is_transparent = vim.g.transparent_background or false
 if is_transparent then

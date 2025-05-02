333ce9944f62fd149db6a193efb9fe9af989e753
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2025-05-02 22:34:31 +0300

    fix issue with copying text inside insert mode

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -43,24 +43,7 @@ return {
 	build = "make",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
-		"stevearc/dressing.nvim",
 		"nvim-lua/plenary.nvim",
 		"MunifTanjim/nui.nvim",
-		{
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				-- recommended settings
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					-- required for Windows users
-					use_absolute_path = true,
-				},
-			},
-		},
 	},
 }

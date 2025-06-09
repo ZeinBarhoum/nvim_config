3ebb4c9242f106bc7cfb95de75148f40337442d5
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2025-06-09 21:25:43 +0300

    fix markdown-preview, make sure yarn is installed

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,7 +1,7 @@
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install & git restore .",
+	build = "cd app && yarn install & git restore .",
 	init = function()
 		vim.g.mkdp_filetypes = { "markdown" }
 	end,

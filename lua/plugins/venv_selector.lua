8927c770812beaf5cea902aef375da7c3be8f72f
Zein Alabedeen Barhoum <zein.barhoum799@gmail.com>
2025-09-01 11:15:47 +0300

    Updae venv_selector config

--- lua/plugins/venv_selector.lua
+++ lua/plugins/venv_selector.lua
@@ -7,19 +7,18 @@ return {
 		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
 	lazy = false,
-	branch = "regexp", -- This is the regexp branch, use this for the new version
 	config = function()
 		require("venv-selector").setup({
 			settings = {
 				search = {
 					micromamba = {
-						command = "fd --full-path 'micromamba/envs/.*/python$' --type symlink ~",
+						command = "fd --full-path '.*/python$' --type symlink ~/micromamba/envs",
 					},
 					mamba = {
-						command = "fd --full-path 'mamba/envs/.*/python$' --type symlink ~",
+						command = "fd --full-path '.*/python$' --type symlink ~/mamba/envs",
 					},
 					conda = {
-						command = "fd --full-path 'conda/envs/.*/python$' --type symlink ~",
+						command = "fd --full-path '.*/python$' --type symlink ~/conda/envs",
 					},
 				},
 			},
